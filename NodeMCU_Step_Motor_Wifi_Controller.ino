a970adbb5cfd4c8516121028ddfb85499bf91f6b
Jean Goetten <34884262+JeanGoetten@users.noreply.github.com>
2020-02-06 11:11:13 -0300

    release motor when idle

--- NodeMCU_Step_Motor_Wifi_Controller.ino
+++ NodeMCU_Step_Motor_Wifi_Controller.ino
@@ -9,10 +9,12 @@ char auth[] = "XvBFoiS0bmFclTcmOx7aK_VCIyzd07py";
 char ssid[] = "StarTec_Jean Carlos";
 char pass[] = "jean12345";
 
+int steppes_per_revolution = 200;
+int steppes_live = 1; 
 int pinValue_speed;
 
-Stepper motorRight(pinValue_speed, D2, D3, D4, D5);
-Stepper motorLeft(pinValue_speed, D5, D6, D7, D8);
+Stepper motorRight(steppes_per_revolution, D1, D2, D3, D4);
+Stepper motorLeft(steppes_per_revolution, D5, D6, D7, D8);
 
 bool left_motor_check, right_motor_check;  
 
@@ -25,7 +27,13 @@ void setup() {
 BLYNK_WRITE(V0){
   
   pinValue_speed = param.asInt(); 
-  
+
+  if(pinValue_speed < 1){
+    pinValue_speed = 1; 
+  }
+
+  motorLeft.setSpeed(pinValue_speed); 
+  motorRight.setSpeed(pinValue_speed);
   
  }
 BLYNK_WRITE(V1){
@@ -52,23 +60,47 @@ BLYNK_WRITE(V2){
     }
   
  }
+ BLYNK_WRITE(V3){
+  
+  int pinValue_backward = param.asInt(); 
+
+  if(pinValue_backward){
+    steppes_live = -1; 
+  }else{
+    steppes_live = 1; 
+  }
+  
+ }
 
 void loop() {
   Blynk.run();
 
   if(left_motor_check){
-    motorLeft.step(pinValue_speed); 
+    motorLeft.step(steppes_live); 
     Serial.print("Motor Left Speed Moment: "); 
     Serial.print(pinValue_speed); 
     Serial.print("\n"); 
     delay(1);
   }
   if(right_motor_check){
-    motorRight.step(pinValue_speed); 
+    motorRight.step(steppes_live); 
     Serial.print("Motor Right Speed Moment: "); 
     Serial.print(pinValue_speed); 
     Serial.print("\n"); 
     delay(1);
+  }else{
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+    digitalWrite(D3, LOW);
+    digitalWrite(D4, LOW);
+    
+    digitalWrite(D5, LOW);
+    digitalWrite(D6, LOW);
+    digitalWrite(D7, LOW);
+    digitalWrite(D8, LOW);
+    Serial.print("Motor released"); 
+    Serial.print("\n"); 
+    delay(1);
   }
-  delay(10); 
+  delay(1); 
 }
